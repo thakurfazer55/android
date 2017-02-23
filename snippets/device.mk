b6e4e9c5f15bc3787c98c52e0ae1d50d5b97fcd8
Diego Muñoz <dimusa.dms@gmail.com>
2017-02-23 02:17:55 +0100

    Add new devices

--- snippets/device.mk
+++ snippets/device.mk
@@ -6,5 +6,9 @@
   <!-- Motorola -->
   <project path="device/motorola/athene" name="LineageOS/android_device_motorola_athene" remote="github" revision="cm-14.1" />
   <project path="kernel/motorola/msm8952" name="athene-caf/platform_kernel_motorola_msm8952" remote="github" revision="n-mr1" />
-  
+  <project path="device/motorola/falcon" name="LineageOS/android_device_motorola_falcon" remote="github" revision="cm-14.1" />
+  <project path="device/motorola/msm8226-common" name="LineageOS/android_device_motorola_msm8226-common" remote="github" revision="cm-14.1" />
+  <project path="kernel/motorola/msm8226" name="LineageOS/android_kernel_motorola_msm8226" remote="github" revision="cm-14.1" />
+  <project path="kernel/motorola/peregrine" name="LineageOS/android_kernel_motorola_msm8226" remote="github" revision="cm-14.1" />
+
 </manifest>
