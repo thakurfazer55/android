2a7c9360d1f657404eef69ff25e2103f7770d193
Diego Muñoz <dimusa.dms@gmail.com>
2017-02-23 02:00:00 +0100

    Start devices

---
+++ snippets/device.mk
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<manifest>
+
+  <!-- AOSGP devices -->
+  
+  <!-- Motorola -->
+  <project path="device/motorola/athene" name="LineageOS/android_device_motorola_athene" remote="github" revision="cm-14.1" />
+  <project path="kernel/motorola/msm8952" name="athene-caf/platform_kernel_motorola_msm8952" remote="github" revision="n-mr1" />
+  
+</manifest>
