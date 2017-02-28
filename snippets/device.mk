092f735f84d76e11a4c1c20716f7ef8579e929cb
Diego Muñoz <dimusa.dms@gmail.com>
2017-02-28 19:24:19 +0100

    Add LG

--- snippets/device.mk
+++ snippets/device.mk
@@ -7,8 +7,21 @@
   <project path="device/motorola/athene" name="LineageOS/android_device_motorola_athene" remote="github" revision="cm-14.1" />
   <project path="kernel/motorola/msm8952" name="athene-caf/platform_kernel_motorola_msm8952" remote="github" revision="n-mr1" />
   <project path="device/motorola/falcon" name="LineageOS/android_device_motorola_falcon" remote="github" revision="cm-14.1" />
+  <project path="device/motorola/peregrine" name="LineageOS/android_device_motorola_peregrine" remote="github" revision="cm-14.1" />
+  <project path="device/motorola/titan" name="LineageOS/android_device_motorola_titan" remote="github" revision="cm-14.1" />
   <project path="device/motorola/msm8226-common" name="LineageOS/android_device_motorola_msm8226-common" remote="github" revision="cm-14.1" />
   <project path="kernel/motorola/msm8226" name="LineageOS/android_kernel_motorola_msm8226" remote="github" revision="cm-14.1" />
-  <project path="kernel/motorola/peregrine" name="LineageOS/android_kernel_motorola_msm8226" remote="github" revision="cm-14.1" />
-
+  <project path="device/motorola/osprey" name="LineageOS/android_device_motorola_osprey" remote="github" revision="cm-14.1" />
+  <project path="device/motorola/msm8916-common" name="LineageOS/android_device_motorola_msm8916-common" remote="github" revision="cm-14.1" />
+  <project path="kernel/motorola/msm8916" name="LineageOS/android_kernel_motorola_msm8916" remote="github" revision="cm-14.1" />
+  
+  
+  <!-- LG -->
+  <project path="device/lge/d855" name="LineageOS/android_device_lge_d855" remote="github" revision="cm-14.1" />
+  <project path="device/lge/g3-common" name="LineageOS/android_device_lge_g3-common" remote="github" revision="cm-14.1" />
+  <project path="kernel/lge/g3" name="LineageOS/android_kernel_lge_g3" remote="github" revision="cm-14.1" />
+  <project path="device/lge/bullhead" name="LineageOS/android_device_lge_bullhead" remote="github" revision="cm-14.1" />
+  <project path="kernel/lge/bullhead" name="LineageOS/android_kernel_lge_bullhead" remote="github" revision="cm-14.1" />
+  
+  
 </manifest>
