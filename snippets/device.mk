002adf451596212039f8f467216b65daf57dc81b
Diego Muñoz <dimusa.dms@gmail.com>
2017-03-01 04:28:54 +0100

    Add Oneplus, Xiaomi and Extras
    To do:
    Add vendor for Motorola
    Fix issues in future

--- snippets/device.mk
+++ snippets/device.mk
@@ -22,6 +22,30 @@
   <project path="kernel/lge/g3" name="LineageOS/android_kernel_lge_g3" remote="github" revision="cm-14.1" />
   <project path="device/lge/bullhead" name="LineageOS/android_device_lge_bullhead" remote="github" revision="cm-14.1" />
   <project path="kernel/lge/bullhead" name="LineageOS/android_kernel_lge_bullhead" remote="github" revision="cm-14.1" />
+  <project path="vendor/lge" name="TheMuppets/proprietary_vendor_lge" remote="github" revision="cm-14.1" />  
   
+  <!-- OnePlus -->
+  <project path="device/oneplus/oneplus3" name="LineageOS/android_device_oneplus_oneplus3" remote="github" revision="cm-14.1" />
+  <project path="device/oppo/common" name="LineageOS/android_device_oppo_common" remote="github" revision="cm-14.1" />
+  <project path="kernel/oneplus/msm8996" name="LineageOS/android_kernel_oneplus_msm8996" remote="github" revision="cm-14.1" />
+  <project path="vendor/oneplus" name="TheMuppets/proprietary_vendor_oneplus" remote="github" revision="cm-14.1" />
   
-</manifest>
+  <!-- Xiaomi -->
+  <project path="device/xiaomi/gemini" name="LineageOS/android_device_xiaomi_gemini" remote="github" revision="cm-14.1" />
+  <project path="kernel/xiaomi/msm8996" name="LineageOS/android_kernel_xiaomi_msm8996" remote="github" revision="cm-14.1" />
+  <project path="device/xiaomi/kenzo" name="LineageOS/android_device_xiaomi_kenzo" remote="github" revision="cm-14.1" />
+  <project path="device/xiaomi/msm8956-common" name="LineageOS/android_device_xiaomi_msm8956-common" remote="github" revision="cm-14.1" />
+  <project path="kernel/xiaomi/msm8956" name="LineageOS/android_kernel_xiaomi_msm8956" remote="github" revision="cm-14.1" />
+  <project path="vendor/xioami" name="TheMuppets/proprietary_vendor_xiaomi" remote="github" revision="cm-14.1" />
+  
+  <!-- Extras -->
+  <project path="device/qcom/common" name="LineageOS/android_device_qcom_common" remote="github" revision="cm-14.1" />
+  <project path="external/bson" name="LineageOS/android_external_bson" remote="github" revision="cm-14.1" />
+  <project path="external/stlport" name="LineageOS/android_external_stlport" remote="github" revision="cm-14.1" />
+  <project path="packages/resources/devicesettings" name="LineageOS/android_packages_resources_devicesettings" remote="github" revision="cm-14.1" />
+  <project path="vendor/nxp-nfc/opensource/frameworks" name="LineageOS/android_vendor_nxp-nfc_opensource_frameworks" remote="github" revision="cm-14.1" />
+  <project path="vendor/nxp-nfc/opensource/libnfc-nci" name="LineageOS/android_vendor_nxp-nfc_opensource_libnfc-nci" remote="github" revision="cm-14.1" />
+  <project path="vendor/nxp-nfc/opensource/Nfc" name="LineageOS/android_vendor_nxp-nfc_opensource_Nfc" remote="github" revision="cm-14.1" />
+  <project path="vendor/qcom/binaries" name="TheMuppets/proprietary_vendor_qcom_binaries" remote="github" revision="cm-14.1" />
+ 
+ </manifest>
